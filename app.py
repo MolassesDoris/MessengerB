aaa89a29411125f2883eb2ccafc919bbd9998918
felix_delos-santos <felix_delos-santos@mpstor.com>
2017-10-03 12:22:14 +0100

    Edit ap

--- app.py
+++ app.py
@@ -13,21 +13,21 @@ PAT = ''
 
 @app.route('/', methods=['GET'])
 def handle_verification():
-    print "Handling Verification."
+    print("Handling Verification.")
     if request.args.get('hub.verify_token') == 'my_voice_is_my_password_verify_me':
-        print "Verification successful!"
-        return request.args.get('hub.challenge')
+        print("Verification successful!")
+        return(request.args.get('hub.challenge'))
     else:
-        print "Verification failed!"
-        return 'Error, wrong validation token'
+        print("Verification failed!")
+        return('Error, wrong validation token')
 
 @app.route('/', methods=['POST'])
 def handle_messages():
-    print "Handling Messages"
+    print("Handling Messages")
     payload = request.get_data()
-    print payload
+    print(payload)
     for sender, message in messaging_events(payload):
-        print "Incoming from %s: %s" % (sender, message)
+        print("Incoming from %s: %s" % (sender, message))
         send_message(PAT, sender, message)
     return "ok"
 
@@ -47,15 +47,15 @@ def messaging_events(payload):
 def send_message(token, recipient, text):
     """Send the message text to recipient with id recipient.
     """
-
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages      params={"access_token": token},
-        data=json.dumps({
-            "recipient": {"id": recipient},
-            "message": {"text": text.decode('unicode_escape')}
-        }),
-        headers={'Content-type': 'application/json'})
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+                      params={"access_token": token},
+                      data=json.dumps({
+                          "recipient": {"id": recipient},
+                          "message": {"text": text.decode('unicode_escape')}
+                      }),
+                      headers={'Content-type': 'application/json'})
     if r.status_code != requests.codes.ok:
-        print r.text
+        print(r.text)
 
 if __name__ == '__main__':
     app.run()
