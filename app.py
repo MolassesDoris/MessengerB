57020f44bb11dbd2aa85cb009e899f735de5cb49
felix_delos-santos <felix_delos-santos@mpstor.com>
2017-10-04 14:24:07 +0100

    fixed typos

--- app.py
+++ app.py
@@ -28,12 +28,11 @@ quick_replies_list = [{
     "content_type":"text",
     "title":"Doggos",
     "payload":"dog",
-    }{
+    },{
     "content_type":"text",
     "title":"BlackPeopleTwitter",
     "payload":"BlackPeopleTwitter",
-    }
-    {
+    },{
     "content_type":"text",
     "title":"Dank Christian Memes",
     "payload":"DankChristianMemes",
