5e6dc8b490665ea7485b55f3e6806f0b5abcb093
felix_delos-santos <felix_delos-santos@mpstor.com>
2017-10-10 17:26:54 +0100

    test

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def handle_messages():
     return("ok")
 
 def handle_whitelisting(token, website):
-    # TODO THIS FUNCTION NEEDS TO BE IMPLEMENTED PROPERLY
+    # TODO THIS FUNCTION NEEDS TO BE IMPLEMENTED PROPERLY.
 
     data = to_json({
         "setting_type": "domain_whitelisting",
